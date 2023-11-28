c346fc6aaef0e9d00f5f10bc267e489b9f413c61
Nason Omasta <na.j.omasta@baypath.net>
2023-11-28 16:13:52 +0000

    Added a carousel to site/index

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "nebulachance-armadadeclare-3000.codio.io"
 
   config.hosts << "passiveegypt-exportmentor-3000.codio.io"
 
