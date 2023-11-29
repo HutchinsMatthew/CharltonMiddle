222ffaef28cff3a6c060209aed90571ef2d9d26a
Joseph Budney <jo.s.budney@baypath.net>
2023-11-29 15:45:21 +0000

    added styles for chat

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_233115) do
     t.integer "club_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "room_id", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -34,6 +44,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_233115) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
+    t.integer "grade"
+    t.decimal "gpa"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -47,6 +59,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_233115) do
 
   create_table "teachers", force: :cascade do |t|
     t.string "name"
+    t.string "subject"
+    t.integer "avgTenure"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -65,6 +79,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_233115) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "messages", "rooms"
-  add_foreign_key "messages", "users"
 end
